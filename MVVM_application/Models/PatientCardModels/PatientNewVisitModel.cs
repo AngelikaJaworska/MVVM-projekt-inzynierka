e41e3644adf9bdef896751568d061fa454d55419
AngelikaJaworska <angelikajaw@onet.pl>
2017-12-05 15:21:15 +0100

    rejestracja na wizyte

--- MVVM_application/Models/PatientCardModels/PatientNewVisitModel.cs
+++ MVVM_application/Models/PatientCardModels/PatientNewVisitModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MVVM_application.Models.PatientCardModels
 {
@@ -11,14 +12,20 @@ namespace MVVM_application.Models.PatientCardModels
     {
         private IManager _manager;
         private Clinic _database;
-        private Visits _visit;
+
         private Patient _patient;
+        private Doctor _doctor;
+        private List<string> _doctorNameList;
+        private List<DateTime> _visitDateList;
 
         public PatientNewVisitModel(IManager manager)
         {
             _manager = manager;
             _database = _manager.GetDatabase();
             _patient = _manager.GetPatient();
+            
+            _doctorNameList = new List<string>();
+            _visitDateList = new List<DateTime>();
         }
 
         public string GetPatientNameAndSurname()
@@ -30,21 +37,7 @@ namespace MVVM_application.Models.PatientCardModels
                 .ToString();
             return nameAndSurname;
         }
-
-        public void SetPatientNameAndSurname(string _nameAndSurname)
-        {
-            if (_patient != null && _nameAndSurname != null && _nameAndSurname != "")
-            {
-                var patient = _nameAndSurname.Split(' ');
-                var name = patient[0];
-                var surname = patient[1];
-
-                _patient.First_Name = name;
-                _patient.Last_Name = surname;
-                _database.SaveChanges();
-            }
-        }
-
+        
         public List<string> FillSpecialisationList()
         {
             var specialisationList = _database.Specialisation
@@ -52,6 +45,159 @@ namespace MVVM_application.Models.PatientCardModels
                             .ToList();
 
             return specialisationList;
+        }
+
+        public List<string> FillDoctorNameList(string specialisationName)
+        {
+            if (specialisationName != null)
+            {
+                var _doctorList = _database.Doctor
+                    .Where(s => s.Specialisation.Name
+                    .Equals(specialisationName))
+                    .ToList();
+
+                foreach (Doctor d in _doctorList)
+                {
+                    _doctorNameList.Add(d.First_Name + " " + d.Last_Name);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Proszę wybrać specjalność");
+            }
+
+            return _doctorNameList;
+        }
+
+        public List<DateTime> FillVisitDateList(string doctor, string specialisation)
+        {
+            if(doctor != null && specialisation != null)
+            {
+                var _doctorName = doctor.Split(' ');
+                var _doctorFirstName = _doctorName[0];
+                var _doctorLastName = _doctorName[1];
+
+                _doctor = _database.Doctor
+                    .Where(d =>
+                (d.First_Name.Equals(_doctorFirstName))
+                && (d.Last_Name
+                .Equals(_doctorLastName)
+                && (d.Specialisation.Name
+                .Equals(specialisation))))
+                .Single();
+
+                foreach (DateTime day in EachDay(DateTime.Today, DateTime.Now.AddDays(4)))
+                {
+                    _visitDateList.Add(day);
+                }
+                _visitDateList = CheckingIfPossible(_visitDateList);
+             }
+            return _visitDateList;
+        }
+        public IEnumerable<DateTime> EachDay(DateTime start, DateTime end)
+        {
+            for (var day = start.Date; day.Date <= end.Date; day = day.AddMinutes(30))
+            {
+                if (day.DayOfWeek != DayOfWeek.Saturday && day.DayOfWeek != DayOfWeek.Sunday && day.DayOfYear != 24.12 && day.DayOfYear != 25.12 && day.DayOfYear != 26.12)
+                {
+                    yield return day;
+                }
+            }
+        }
+
+        public List<DateTime> CheckingIfPossible(List<DateTime> date)
+        {
+            Doctor doctor = _database.Doctor.Select(s => s).Where(i => i.IDDoctor == _doctor.IDDoctor).Single();
+            TimeSpan startTime = doctor.WorkStart;
+            TimeSpan endTime = doctor.WorkEnd;
+            List<Visits> thisDoctorVisit = _database.Visits.Select(s => s).Where(i => i.IDDoctor == _doctor.IDDoctor).ToList();
+            List<Visits> visits = _database.Visits.Select(s => s).ToList();
+
+            List<DateTime> possibleVisitsList = new List<DateTime>();
+
+            foreach (DateTime dt in date)
+            {
+                possibleVisitsList.Add(dt);
+            }
+
+            foreach (DateTime d in date)
+            {
+                if (d.TimeOfDay < startTime || d.TimeOfDay >= endTime)  //czy w godzinach pracy lekarza
+                {
+                    possibleVisitsList.Remove(d);
+                }
+
+                if (DateTime.Now.TimeOfDay > d.TimeOfDay && DateTime.Now.Date == d.Date) //czy dana godz juz minela
+                {
+                    possibleVisitsList.Remove(d);
+                }
+                foreach (Visits v in thisDoctorVisit)
+                {
+                    if (v.VisitDate.TimeOfDay == d.TimeOfDay && v.VisitDate.Date == d.Date) //czy taka wizyta jest juz zajeta
+                    {
+                        possibleVisitsList.Remove(d);
+                    }
+                    if (v.IDPatient == _patient.IDPatient && v.VisitDate.Date == d.Date) //czy pacjent juz w tym dniu jest zapisany na wizyte
+                    {
+                        possibleVisitsList.Remove(d);
+                    }
+                }
+                foreach (Visits vv in visits)
+                {
+                    if (vv.VisitDate.TimeOfDay == d.TimeOfDay && vv.VisitDate.Date == d.Date && vv.IDPatient == _patient.IDPatient) //czy pacjent juz do innego lekarza o tej godz
+                    {
+                        possibleVisitsList.Remove(d);
+                    }
+                }
+            }
+            return possibleVisitsList;
+
+
+        }
+
+        internal bool CreateVisitWithData(string nameAndSurname, string specialisationName, 
+            string doctorName, string visitDate, string comments)
+        {
+            if (nameAndSurname != null && nameAndSurname != "" 
+                && specialisationName != null && specialisationName != "" 
+                && doctorName != null && doctorName != "" 
+                && visitDate != null && visitDate != "" )
+            {
+                var _patientName = nameAndSurname.Split(' ');
+                var _patientFirstName = _patientName[0];
+                var _patientLastName = _patientName[1];
+
+                var _doctorName = doctorName.Split(' ');
+                var _doctorFirstName = _doctorName[0];
+                var _doctorLastName = _doctorName[1];
+
+                var patient = _database.Patient
+                    .Where(p => (p.First_Name.Equals(_patientFirstName))
+                    && (p.Last_Name.Equals(_patientLastName)))
+                    .Single();
+
+                var doctor = _database.Doctor
+                    .Where(d => (d.First_Name.Equals(_doctorFirstName))
+                    && (d.Last_Name.Equals(_doctorLastName))
+                    && (d.Specialisation.Name.Equals(specialisationName)))
+                    .Single();
+
+                Visits _visit = new Visits();
+                _visit.IdReceptionist = _manager.GetReceptionist().IDReceptionist;
+                _visit.IDPatient = patient.IDPatient;
+                _visit.IDDoctor = doctor.IDDoctor;
+                _visit.VisitDate = DateTime.Parse(visitDate);
+                _visit.Comments = comments;
+
+                _database.Visits.Add(_visit);
+                _database.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- MVVM_application/ViewModels/PatientCardViewModels/PatientNewVisitViewModel.cs
+++ MVVM_application/ViewModels/PatientCardViewModels/PatientNewVisitViewModel.cs
@@ -6,6 +6,7 @@ using System.Windows.Input;
 using System;
 using System.Windows;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace MVVM_application.ViewModels.PatientCardViewModels
 {
@@ -15,8 +16,15 @@ namespace MVVM_application.ViewModels.PatientCardViewModels
         private readonly PatientNewVisitModel _patientNewVisitModel;
 
         private Patient _patient;
+        private List<string> _doctorNameList;
+        private List<DateTime> _visitDateList;
 
         private string _nameAndSurname;
+        private string _specialisationName;
+        private string _doctorName;
+        private string _visitDate;
+        private string _comments;
+
         public string NameAndSurname
         {
             get { return _nameAndSurname; }
@@ -26,47 +34,133 @@ namespace MVVM_application.ViewModels.PatientCardViewModels
                 RaisePropertyChanged("NameAndSurname");
             }
         }
+        public string SpecialisationName
+        {
+            get { return _specialisationName; }
+            set
+            {
+                _specialisationName = value;
+                RaisePropertyChanged("SpecialisationName");
+            }
+        }
+        public string DoctorName
+        {
+            get { return _doctorName; }
+            set
+            {
+                _doctorName = value;
+                RaisePropertyChanged("DoctorName");
+            }
+        }
+        public string VisitDate
+        {
+            get { return _visitDate; }
+            set
+            {
+                _visitDate = value;
+                RaisePropertyChanged("VisitDate");
+            }
+        }
+        public string Comments
+        {
+            get { return _comments; }
+            set
+            {
+                _comments = value;
+                RaisePropertyChanged("Comments");
+            }
+        }
 
-        public ObservableCollection<string> SpecialisationtList { get; set; }
+        public ObservableCollection<string> SpecialisationtNameList { get; set; }
+        public ObservableCollection<string> DoctorNameList { get; set; }
+        public ObservableCollection<DateTime> VisitDateList { get; set; }
 
         public ICommand SaveCommand { get; private set; }
         public ICommand CancelCommand { get; private set; }
+        public RelayCommand RefreshDoctorCommand { get; private set; }
+        public RelayCommand RefreshVisitDateCommand { get; private set; }
 
         public PatientNewVisitViewModel(IManager manager, PatientNewVisitModel patientNewVisitModel)
         {
             _manager = manager;
             _patientNewVisitModel = patientNewVisitModel;
             _patient = _manager.GetPatient();
+
             if(_patient != null)
             {
                 FillData();
             }
+
+            InitialiseCommand();
+        }
+
+        private void FillData()
+        {
+            _nameAndSurname = _patientNewVisitModel.GetPatientNameAndSurname();
+            SpecialisationtNameList = new ObservableCollection<string>(_patientNewVisitModel.FillSpecialisationList());
+            DoctorNameList = new ObservableCollection<string>();
+            VisitDateList = new ObservableCollection<DateTime>();
+            _doctorNameList = new List<string>();
+            _visitDateList = new List<DateTime>();
+        }
+
+        private void InitialiseCommand()
+        {
             SaveCommand = new RelayCommand(ExecuteSaveCommand);
             CancelCommand = new RelayCommand(ExecuteCancelCommand);
+            RefreshDoctorCommand = new RelayCommand(ExecuteRefreshDoctorCommand);
+            RefreshVisitDateCommand = new RelayCommand(ExecuteRefreshVisitDateCommand);
+        }
+
+        private void ExecuteSaveCommand()
+        {
+            if (_patientNewVisitModel.CreateVisitWithData(_nameAndSurname, _specialisationName, _doctorName, _visitDate, _comments))
+            {
+                MessageBox.Show("Wizyta zapisana");
+                this.SpecialisationtNameList.Clear();
+                this.DoctorNameList.Clear();
+                this.VisitDateList.Clear();
+            }
+                else
+            {
+                MessageBox.Show("Prosze uzupelnic dane");
+            }
         }
 
         private void ExecuteCancelCommand()
         {
             MessageBox.Show("Anulowanie");
+            this.SpecialisationtNameList.Clear();
+            this.DoctorNameList.Clear();
+            this.VisitDateList.Clear();
         }
 
-        private void ExecuteSaveCommand()
+        private void ExecuteRefreshDoctorCommand()
         {
-            SetData();
-            MessageBox.Show("Dane prawidlowo zmienione");
+            if (_specialisationName != null)
+            {
+                _doctorNameList.Clear();
+                _doctorNameList = _patientNewVisitModel.FillDoctorNameList(_specialisationName);
+                this.DoctorNameList.Clear();
+                for (int i = 0; i < _doctorNameList.Count; i++)
+                {
+                    this.DoctorNameList.Add(_doctorNameList[i]);
+                }
+            }
         }
 
-        private void SetData()
+        private void ExecuteRefreshVisitDateCommand()
         {
-            _patientNewVisitModel.SetPatientNameAndSurname(_nameAndSurname);
-            
-        }
-
-        private void FillData()
-        {
-           _nameAndSurname =  _patientNewVisitModel.GetPatientNameAndSurname();
-
-            SpecialisationtList = new ObservableCollection<string>(_patientNewVisitModel.FillSpecialisationList());
-        }
+            if (_doctorName != null && _specialisationName != null)
+            {
+                _visitDateList.Clear();
+                _visitDateList = _patientNewVisitModel.FillVisitDateList(_doctorName, _specialisationName);
+                this.VisitDateList.Clear();
+                for (int i = 0; i < _visitDateList.Count; i++)
+                {
+                    this.VisitDateList.Add(_visitDateList[i]);
+                }
+            }
+        }       
     }
 }
