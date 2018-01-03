b0c4d81e19261f5066aba0f7365ee30b1668eca6
AngelikaJaworska <angelikajaw@onet.pl>
2018-01-03 14:56:29 +0100

    Front cz.1
    zainstalowana biblioteka, logowanie, daily

--- MVVM_application/MainWindow.xaml.cs
+++ MVVM_application/MainWindow.xaml.cs
@@ -13,12 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MahApps.Metro.Controls;
+
 namespace MVVM_application
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
